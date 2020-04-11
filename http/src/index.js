60eea3883fefa89b24711a3b51e96985ebfb4405
macpaik <macxpaik@gmail.com>
2020-04-11 21:22:53 +0900

    added axios interceptor

--- http/src/index.js
+++ http/src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+const requestInterceptor = axios.interceptors.request.use(request => {
+    console.log(request);
+    // Edit request config
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+axios.interceptors.request.eject(requestInterceptor);
+
+const responseInterceptor = axios.interceptors.response.use(response => {
+    console.log(response);
+    // Edit request config
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+axios.interceptors.request.eject(responseInterceptor);
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
