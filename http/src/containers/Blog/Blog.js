06712469608de587ba7a55b4334e59cef2e9d2bd
macpaik <macxpaik@gmail.com>
2020-04-12 16:39:48 +0900

    improved router style

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -16,7 +16,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={() => <Posts /> } />
+                {/*<Route path="/" exact render={() => <Posts /> } />*/}
+                <Route path="/" exact component={Posts} />
             </div>
         );
     }
