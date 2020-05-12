83390266f59cfcc4743026b517f64470dd9fb130
macpaik <macxpaik@gmail.com>
2020-05-12 22:00:39 +0900

    use function

--- redux-start/src/store/reducers/result.js
+++ redux-start/src/store/reducers/result.js
@@ -5,6 +5,11 @@ const initialState = {
     results: []
 };
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, {results: updatedArray});
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
@@ -12,8 +17,8 @@ const reducer = (state = initialState, action) => {
         case actionTypes.DELETE_RESULT:
             // const newArray = [...state.results];
             // newArray.splice(id, 1);
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return updateObject(state, {results: updatedArray});
+
+            return deleteResult(state, action);
     }
 
     return state;
