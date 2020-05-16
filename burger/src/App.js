9673608a8e25df89ef095bfffbc3049bd3533618
macpaik <macxpaik@gmail.com>
2020-05-16 14:59:05 +0900

    upgraded project

--- burger/src/App.js
+++ burger/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import asyncComponent from "./hoc/asyncComponent/asyncComponent";
 
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
+
+const asyncCheckout = asyncComponent(() => {
+    return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+    return import('./containers/Orders/Orders');
+});
 
 function App() {
   return (
     <div>
       <Layout>
           <Switch>
-              <Route path="/checkout" component={Checkout} />
-              <Route path="/orders" component={Orders} />
+              <Route path="/checkout" component={asyncCheckout} />
+              <Route path="/orders" component={asyncOrders} />
               <Route path="/" exact component={BurgerBuilder} />
           </Switch>
       </Layout>
