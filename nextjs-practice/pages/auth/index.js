4dda9857b2d6ba1bc9c327b08a21f014d4162f11
macpaik <macxpaik@gmail.com>
2020-05-16 16:33:49 +0900

    added some pages

--- nextjs-practice/pages/auth/index.js
+++ nextjs-practice/pages/auth/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import User from '../../components/User';
+
 const authindexPage = () => (
     <div>
         <h1>The Auth Main Page</h1>
+        <User name="mac" age="30" />
     </div>
 );
 
